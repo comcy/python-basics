37b8e3b61a1183001c65fa3f82a70ef4b41cd733
comcy <christian.silfang@gmail.com>
2021-11-30 10:02:51 +0100

    added some samples

---
+++ 01/hello_world.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print('Hello World')
+print('A', "B", 1, 2*2, "2*2")
+
+# 80 Sterne
+print("*" *80)
+
+# Neuer Trenner
+print("A", "B", "C", sep=':')
+
+# Ende 
+print("A", "B", "C", end='!')
+
+# Steuercodes
+print("\n")
+print("\n", "ABC", "\r", "BCD", "\t", "CDE")
